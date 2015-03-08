c45b977b0ec1335f97f956a0697dde87e7739d9e
g <G@Gs-iMac-2.local>
2015-03-08 18:11:22 +0900

    최근

--- html/default.js
+++ html/default.js
@@ -1,4 +1,6 @@
 jQuery(function($){
+
+	// 모바일 GNB 토글 버튼
 	var $gnbBtn = $(".gnb>button");
 
 	$gnbBtn.on("click", function(){
@@ -12,4 +14,10 @@ jQuery(function($){
 			$gnbBtn.text("메뉴 열기");
 		}
 	});
+
+	// 태블릿 + 데스크탑 GNB 메뉴
+	$(".gnb>ul>li>a").on("mouseover focus", function(){
+		$(this).parent().addClass("active").siblings().removeClass("active");
+	});
+	// li가 아니라 a를 잡아서 하는 이유는, 키보드로 focus를 받을 수 없기 때문이다. a, input같은 것만 받을 수 있다.
 });
