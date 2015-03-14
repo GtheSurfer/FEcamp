e6268976ac65c3f8336799ebe19c5ca32ec41b3b
GtheSurfer <jibong.koo@gmail.com>
2015-03-14 09:16:36 +0900

    3.14.01

---
+++ javaScript/js/day1.js
@@ -0,0 +1,11 @@
+var str = 'Hello, world!';
+console.log(str);
+
+var num1 = 100, num2 = 20;
+console.log( 5%2 );
+
+var str1 = 'Hello', str2 = 'world!', str3 = ' ';
+console.log( str1 + ',' + str3 + str2 + str3 + num1);
+
+var 삼백 = '300';
+console.log = (typeof 삼백);
